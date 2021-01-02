637534519d6aebb5e2ea71ef0627d4b901e2e7d8
Roman Govor 850503 <roma-govor2013@ya.ru>
2021-01-03 02:24:31 +0300

    feat: add menu and canvas of playground

--- rsclone/src/index.js
+++ rsclone/src/index.js
@@ -1,1 +1,31 @@
-console.log(45);
+class App {
+  constructor() {
+    this.language = 'en';
+    this.setEvents();
+  }
+
+  setEvents() {
+    const switchGameModeBtn = document.querySelector('.switch__checkbox');
+    switchGameModeBtn.addEventListener('change', () => {
+      const switchEn = document.querySelector('.switch__en');
+      const switchRu = document.querySelector('.switch__ru');
+      switchRu.classList.toggle('none');
+      switchEn.classList.toggle('none');
+
+      this.translateStrings();
+    });
+  }
+
+  translateStrings() {
+    const enStings = document.querySelectorAll('[language="en"]');
+    const ruStings = document.querySelectorAll('[language="ru"]');
+    enStings.forEach((el) => {
+      el.classList.toggle('none');
+    });
+    ruStings.forEach((el) => {
+      el.classList.toggle('none');
+    });
+  }
+}
+
+const APP = new App();
---
+++ rsclone/src/js/Level.js
@@ -0,0 +1,5 @@
+export class Level {
+  constructor(lang) {
+    this.language = lang;
+  }
+}
